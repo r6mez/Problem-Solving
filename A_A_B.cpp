b42f881270ccf8fe5a3b5cb1aaf7afead4d36eda
Ramez Medhat <iramezdev@gmail.com>
2024-05-02 05:44:07 +0200

    a

---
+++ A_A_B.cpp
@@ -0,0 +1,36 @@
+// ﷽
+// problem: A. A+B?
+// URL: https://codeforces.com/problemset/problem/1772/A  
+// Start: 5/2/2024, 5:21:43 AM
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <cstring>
+#include <string>
+#include <stack>
+#include <queue>
+#include <map>
+#include <set>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define vi vector<int>
+#define vll vector<long long>
+
+void solve(){
+    int a,b; char cc;
+    cin >> a >> cc >> b;
+    cout << a + b << "\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); 
+    cin.tie(nullptr);
+    int t = 1; 
+    cin >> t;
+    while (t--){
+        solve();
+    }
+    return 0;
+}
