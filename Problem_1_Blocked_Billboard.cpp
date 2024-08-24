84f91014990f3f0c58c574ebdb338f7710186780
Ramez Medhat <iramezdev@gmail.com>
2024-08-24 18:36:05 +0300

    USACO Graph + Some geometry

---
+++ Problem_1_Blocked_Billboard.cpp
@@ -0,0 +1,224 @@
+// -------------------- Ramez -------------------------
+// سَأَحمِلُ روحي عَلى راحَتي    وَأُلقي بِها في مَهاوي الرَدى
+// فَإِمّـا حَــيــاةٌ تُسِــرُّ الـصَديقَ    وَإِمّــا مَمــاتٌ يُغــيظُ العِــدى
+// ----------------------------------------------------
+// problem: Problem 1. Blocked Billboard
+// URL: https://usaco.org/index.php?page=viewproblem2&cpid=759  
+// Start: 8/24/2024, 6:30:39 PM
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <climits>
+#include <numeric>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <map>
+#include <set>
+#include <bitset>
+#include <unordered_map>
+#define ll   long long
+#define ull  unsigned long long
+#define vi   vector<int>
+#define vvi  vector<vi>
+#define vll  vector<long long>
+#define vvll vector<vll>
+#define vs   vector<string>
+#define pii  pair<int, int>
+#define pll  pair<ll, ll>
+#define F first
+#define S second
+using namespace std;
+struct   SegTree;
+const int MOD = 1000000007;
+#define all(v)  v.begin(), v.end()
+#define makeUnique(v)  v.erase(unique(all(v)), v.end())
+vector<vll> prefixSum2D(vector<vll>& a);
+ll sumOfSquare(int x1, int y1, int x2, int y2, vector<vll>& a);
+template<typename T> ostream& operator<<(ostream& os, const vector<T>& v);
+template<typename T> istream& operator>>(istream& is, vector<T>& v);
+void FastIO() { ios_base::sync_with_stdio(false); cin.tie(nullptr); }
+void UseFile() { freopen("billboard.in", "r", stdin); freopen("billboard.out", "w", stdout); }
+
+struct Rect {
+	int x1, y1, x2, y2;
+	int area() { return (y2 - y1) * (x2 - x1); }
+};
+
+int intersect(Rect p, Rect q) {
+	int xOverlap = max(0, min(p.x2, q.x2) - max(p.x1, q.x1));
+	int yOverlap = max(0, min(p.y2, q.y2) - max(p.y1, q.y1));
+	return xOverlap * yOverlap;
+}
+
+
+
+int main() {
+    UseFile();
+    FastIO();
+
+    Rect a, b, t;  // billboards a, b, and the truck
+	cin >> a.x1 >> a.y1 >> a.x2 >> a.y2;
+	cin >> b.x1 >> b.y1 >> b.x2 >> b.y2;
+	cin >> t.x1 >> t.y1 >> t.x2 >> t.y2;
+	cout << a.area() + b.area() - intersect(a, t) - intersect(b, t) << endl;
+
+    return 0;
+}
+
+/*
+NOTES:
+
+*/
+
+class SegmentTree {
+public:
+    int size;
+    vector<ll> sums, mins, maxs;
+
+    SegmentTree(int n) {
+        size = 1;
+        while (size < n) size *= 2;
+        sums.assign(2 * size, 0LL);
+        mins.assign(2 * size, LLONG_MAX);
+        maxs.assign(2 * size, LLONG_MIN);
+    }
+
+    void build(const vi& a) {
+        build(a, 0, 0, size);
+    }
+
+    void update(int i, int v) {
+        update(i, v, 0, 0, size);
+    }
+
+    ll sumSeg(int l, int r) {
+        return sumSeg(l, r, 0, 0, size);
+    }
+
+    ll minSeg(int l, int r) {
+        return minSeg(l, r, 0, 0, size);
+    }
+
+    ll maxSeg(int l, int r) {
+        return maxSeg(l, r, 0, 0, size);
+    }
+
+    ll firstElementAtLeastX(int x) {
+        return firstElementAtLeastX(x, 0, 0, size);
+    }
+
+private:
+    void build(const vi& a, int x, int lx, int rx) {
+        if (rx - lx == 1) {
+            if (lx < (int)a.size()) {
+                sums[x] = a[lx];
+                mins[x] = a[lx];
+                maxs[x] = a[lx];
+            }
+            return;
+        }
+        int mid = (lx + rx) / 2;
+        build(a, 2 * x + 1, lx, mid);
+        build(a, 2 * x + 2, mid, rx);
+        sums[x] = sums[2 * x + 1] + sums[2 * x + 2];
+        mins[x] = min(mins[2 * x + 1], mins[2 * x + 2]);
+        maxs[x] = max(maxs[2 * x + 1], maxs[2 * x + 2]);
+    }
+
+    void update(int i, int v, int x, int lx, int rx) {
+        if (rx - lx == 1) {
+            sums[x] = v;
+            mins[x] = v;
+            maxs[x] = v;
+            return;
+        }
+        int mid = (lx + rx) / 2;
+        if (i < mid) {
+            update(i, v, 2 * x + 1, lx, mid);
+        }
+        else {
+            update(i, v, 2 * x + 2, mid, rx);
+        }
+        sums[x] = sums[2 * x + 1] + sums[2 * x + 2];
+        mins[x] = min(mins[2 * x + 1], mins[2 * x + 2]);
+        maxs[x] = max(maxs[2 * x + 1], maxs[2 * x + 2]);
+    }
+
+    ll sumSeg(int l, int r, int x, int lx, int rx) {
+        if (lx >= r || l >= rx) return 0;
+        if (lx >= l && rx <= r) return sums[x];
+        int mid = (lx + rx) / 2;
+        ll left = sumSeg(l, r, 2 * x + 1, lx, mid);
+        ll right = sumSeg(l, r, 2 * x + 2, mid, rx);
+        return left + right;
+    }
+
+    ll minSeg(int l, int r, int x, int lx, int rx) {
+        if (lx >= r || l >= rx) return LLONG_MAX;
+        if (lx >= l && rx <= r) return mins[x];
+        int mid = (lx + rx) / 2;
+        ll left = minSeg(l, r, 2 * x + 1, lx, mid);
+        ll right = minSeg(l, r, 2 * x + 2, mid, rx);
+        return min(left, right);
+    }
+
+    ll maxSeg(int l, int r, int x, int lx, int rx) {
+        if (lx >= r || l >= rx) return LLONG_MIN;
+        if (lx >= l && rx <= r) return maxs[x];
+        int mid = (lx + rx) / 2;
+        ll left = maxSeg(l, r, 2 * x + 1, lx, mid);
+        ll right = maxSeg(l, r, 2 * x + 2, mid, rx);
+        return max(left, right);
+    }
+
+
+    ll firstElementAtLeastX(int x, int currentNode, int l, int r) {
+        if (maxs[currentNode] < x) {
+            return -1;
+        }
+
+        if (r - l == 1) { // we're in the bottom of the tree
+            return l;
+        }
+
+        int mid = (l + r) / 2;
+        int res = firstElementAtLeastX(x, 2 * currentNode + 1, l, mid);
+        if (res == -1) {
+            res = firstElementAtLeastX(x, 2 * currentNode + 2, mid, r);
+        }
+        return res;
+    }
+};
+
+template<typename T> ostream& operator<<(ostream& os, const vector<T>& v) {
+    for (const auto& i : v) os << i << ' ';
+    return os;
+}
+
+template<typename T> istream& operator>>(istream& is, vector<T>& v)
+{
+    for (auto& i : v) is >> i;
+    return is;
+}
+
+vector<vll> prefixSum2D(vector<vll>& a) {
+    vector<vll> b(a.size(), vll(a[0].size()));
+    for (int i = 0; i < a.size(); i++) {
+        partial_sum(all(a[i]), b[i].begin());
+    }
+    for (int i = 0; i < a[0].size(); i++)
+    {
+        for (int j = 1; j < a.size(); j++)
+        {
+            b[j][i] += b[j - 1][i];
+        }
+    }
+    return b;
+}
+
+ll sumOfSquare(int x1, int y1, int x2, int y2, vector<vll>& a) {
+    return (a[x2][y2] - a[x1 - 1][y2] - a[x2][y1 - 1]) + a[x1 - 1][y1 - 1];
+}
