adb7baf87dabbcdc8d3a4fb46c951af7a9ad7f92
Ramez Medhat <iramezdev@gmail.com>
2025-01-08 16:44:07 +0200

    Daily 1500

---
+++ Problems/D_Playoff.cpp
@@ -0,0 +1,42 @@
+/*
+I tought I could bring an end to the world suffering,
+But when every equation was solved
+all it remained were fields of dreamless solitude.
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll   long long
+#define vi   vector<int>
+#define vll  vector<ll>
+#define pll  pair<ll, ll>
+#define all(v)  v.begin(), v.end()
+template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+void FastIO() { ios_base::sync_with_stdio(false); cin.tie(nullptr); }
+void UseFile() { freopen("file.in", "r", stdin); freopen("file.out", "w", stdout); }
+int MOD = 1000000007;
+
+void Ramez() {
+    int n; cin >> n;
+    string s; cin >> s;
+
+    int k = count(s.begin(), s.end(), '1');
+    for (int x = 1 << k; x <= (1 << n) - (1 << (n - k)) + 1; ++x)
+        cout << x << ' ';
+}
+
+/*
+NOTES:
+ the answer to the problem is the segment of integers [2^x,2^n−2^y+1]
+ x number of 1's and y number of 0's
+*/
+
+int main() {
+    // UseFile();
+    FastIO();
+    int t = 1;
+    // cin >> t;
+    while (t--) Ramez();
+    return 0;
+}
+
