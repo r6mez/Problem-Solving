ad3dd52276217fbd4c700f8ae30c4f288f3b1254
Ramez Medhat <iramezdev@gmail.com>
2026-01-20 19:55:42 +0200

    20/01/2026

--- Problems/G_Mixing_MEXes.cpp
+++ Problems/G_Mixing_MEXes.cpp
@@ -15,7 +15,61 @@ void UseFile() { freopen("file.in", "r", stdin); freopen("file.out", "w", stdout
 const int MOD = 1000000007, INF = 2e18;
 
 void solve() {
-    
+    int n; cin >> n;
+
+    vi lengths(n);
+    vector<vi> arrays(n);
+    map<int, int> count;
+    for (int i = 0; i < n; i++) {
+        int length;
+        cin >> length;
+        lengths[i] = length;
+        arrays[i] = vi(lengths[i]);
+        for (auto& x : arrays[i]) {
+            cin >> x;
+            count[x]++;
+        }
+        sort(arrays[i].begin(), arrays[i].end());
+    }
+
+    int mex_sum = 0;
+    vi mexes(n);
+    vi second_mexes(n);
+
+    // Calculate mex and second mex for each array
+    for (int i = 0; i < n; i++) {
+        int current_mex = 0;
+        for (int j = 0; j < lengths[i]; j++) {
+            if (arrays[i][j] > current_mex) break;
+            if (arrays[i][j] == current_mex) current_mex++;
+        }
+        int second_mex = current_mex + 1;
+        for (int j = 0; j < lengths[i]; j++) {
+            if (arrays[i][j] > second_mex) break;
+            if (arrays[i][j] == second_mex) second_mex++;
+        }
+        mexes[i] = current_mex;
+        second_mexes[i] = second_mex;
+        mex_sum += current_mex;
+    }
+
+    int total = 0;
+    // Calculate removals
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < lengths[i]; j++) {
+            int org = total;
+            bool is_alone = (j == 0 || arrays[i][j - 1] != arrays[i][j]) && (j == lengths[i] - 1 || arrays[i][j + 1] != arrays[i][j]);
+            if (is_alone && arrays[i][j] < mexes[i]) total += ((mex_sum - mexes[i] + arrays[i][j])) * (n - 1);
+            else total += (mex_sum) * (n - 1);
+        }
+    }
+
+    // Calculate additions
+    for (int i = 0; i < n; i++) {
+        total += (count[mexes[i]]) * (second_mexes[i] - mexes[i]);
+    }
+
+    cout << total << "\n";
 }
 
 /*
@@ -32,13 +86,12 @@ if we moved x to array with mex y
 if the original array has another x or mex < x, mex stays the same
 
 
-
 */
 
 signed main() {
     // UseFile();
     FastIO();
     int t = 1;
-    // cin >> t;
+    cin >> t;
     while (t--) solve();
 }
